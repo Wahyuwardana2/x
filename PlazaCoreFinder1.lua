35b54424bc9968d866c3e8c4aa4f8bfc5400578e
Wahyuwardana2 <121289117+Wahyuwardana2@users.noreply.github.com>
2026-09-01 12:34:35 +0700

    Rename W FINDER to PLAZA SCANNER throughout file

--- PlazaCoreFinder1.lua
+++ PlazaCoreFinder1.lua
@@ -1,5 +1,5 @@
 --================================================--
--- W FINDER
+-- PLAZA SCANNER
 -- CORE + CONFIG + RAP + FILTER + SCANNER + HOP
 --================================================--
 
@@ -1231,7 +1231,7 @@ local function BuildItemText(item)
 	end
 
 	if item.Mutation and item.Mutation ~= "" then
-		text ..= "Mutation : " .. item.Mutation .. "\n"
+		text ..= "Mutation : *" .. item.Mutation .. "*\n"
 	end
 
 	if item.Size and item.Size ~= "" then
@@ -1246,7 +1246,7 @@ local function BuildItemText(item)
 	end
 
 	text ..=
-		"\nPrice : " .. tostring(item.Price or 0) ..
+		"\nPrice : *" .. tostring(item.Price or 0) .. "*" ..
 		"\nRAP : " .. tostring(item.RAP or "-") .. "\n"
 
 	if item.UnderRap then
@@ -1318,11 +1318,12 @@ local function SendWebhook(items)
 			jobId
 
 		local payload = {
-			username = "W FINDER",
+			username = "PLAZA SCANNER BOT",
+			avatar_url = "https://imgcdn.dev/i/Y8Qko2",
 
 			embeds = {{
 				title =
-					"🎣 W FINDER FOUND (" ..
+					"🎣 PLAZA SCANNER FOUND (" ..
 					#list ..
 					" ITEMS)",
 
@@ -1366,7 +1367,7 @@ local function SendWebhook(items)
 				},
 
 				footer = {
-    text = "W FINDER | " .. game.JobId .. " | " .. GetWIBTime()
+    text = "PLAZA SCANNER | " .. game.JobId .. " | " .. GetWIBTime()
 }
 			}}
 		}
@@ -1395,6 +1396,7 @@ local function SendWebhook(items)
 		end
 	end
 end
+
 
 --================================================--
 -- SERVER CACHE
@@ -1671,7 +1673,7 @@ end
 
 local function StartFinder()
 
-	print("🎣 W FINDER START")
+	print("🎣 PLAZA SCANNER START")
 
 	while true do
 
@@ -1709,4 +1711,4 @@ LoadServerCache()
 
 task.spawn(StartFinder)
 
-print("🎣 W FINDER FISHIT READY")
+print("🎣 PLAZA SCANNER FISHIT READY")
