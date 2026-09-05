50468de60e5f8b936fc0fa6cb718b9cd4d04d20a
Wahyuwardana2 <121289117+Wahyuwardana2@users.noreply.github.com>
2026-09-05 14:52:03 +0700

    Update PlazaCoreFinder.lua

--- PlazaCoreFinder.lua
+++ PlazaCoreFinder.lua
@@ -34,7 +34,7 @@ local Config = {
 
 	Webhook = {
 		Fish = "https://discordapp.com/api/webhooks/1530612875272654961/u2FmJrJssywDYVh-5dWguka_5fpZkyzoCbioZhS-ctNIjYTTJ-1rIppT2hDXXmdbwsnn",
-		Gears = "https://discordapp.com/api/webhooks/1530613045020069991/8gEwiqeGmaCus3ZzWYr14HVWOs5GdX0SC9Iyqevqj6fIDzOB37ozMEXUI1WxvjsbI46e",
+		Gears = "https://discordapp.com/api/webhooks/1545702465297449053/Chshx2Ate62mesZfZKsZ6ldGp8XcbWekE4yj1ZzkCSmAIEdCH8XRsOgeT7OoHigaxRYN",
 		["Fishing Rods"] = "https://discordapp.com/api/webhooks/1530613393306685612/1hBlQwnCzjbVdfQYR1RzfB07e64wDxBPcMCPRM1hFprZQDdHYqYZFHO82L5ZFQDi3aoq",
 		Boats = "https://discordapp.com/api/webhooks/1530613586525815027/osI8YKDYkl_bwCCVT4S6-W-bblZMZ9gTRLhVfLseGuxWIAwz2EN1vpNz1Jl1IisVlPDX",
 		Pets = "https://discordapp.com/api/webhooks/1531668953816891493/myibJmPBQBA_0W3dICiFklJxX_h8ZUyawZVge1cZR9T2UlvLqgU1Mj7mK7e3VkZBL8Lx"
@@ -111,7 +111,7 @@ local Config = {
 			Price = {
 				Enabled = true,
 				Min = 1,
-				Max = 37
+				Max = 48
 			},
 
 			RAP = {
