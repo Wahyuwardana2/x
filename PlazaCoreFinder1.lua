17fafb1376df74279268623bb05558bdd9aa86a1
Wahyuwardana2 <121289117+Wahyuwardana2@users.noreply.github.com>
2026-09-06 22:16:14 +0700

    Update PlazaCoreFinder1.lua

--- PlazaCoreFinder1.lua
+++ PlazaCoreFinder1.lua
@@ -526,11 +526,11 @@ Name = {
 
     Mode = "Whitelist", -- Whitelist / Blacklist
 
-    Match = "Exact", -- Exact / Contains / StartsWith / EndsWith
+    Match = "Contains", -- Exact / Contains / StartsWith / EndsWith
 
     List = {
-			"Runic Enchant Stone",
-			"Eggy Enchant Stone"
+			"Runic",
+			"Eggy"
 
     }
 
